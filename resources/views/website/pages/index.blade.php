3eaa567ee33606c3e4ec51fa160595cffcdb63da
maazhabib <maazhabib73@gamil.com>
2023-10-26 00:06:23 +0500

    working

--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -4,43 +4,43 @@
 
     <div id="main-content">
         <div class="container">
-            <div class="row">~
+            <div class="row">
                 <div class="col-md-8">
                     <!-- post-container -->
                     <div class="post-container">
                         @foreach ($post as $dat)
-
+                            @if ($dat && is_object($dat))
                                 <div class="post-content">
                                     <div class="row">
-                                    <div class="col-md-4">
-                                        <a class="post-img" href=""><img src="{{ asset('images/' . $dat->image)}}" alt="error"/></a>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="inner-content clearfix">
-                                            <h3><a href='single.php'>{{ $dat->title }}</a></h3>
-                                            <div class="post-information">
-                                                <span>
-                                                    <i class="fa fa-tags" aria-hidden="true"></i>
-                                                    <a href='category.php'>{{ $dat->categories->categories_name }}</a>
-                                                </span>
-                                                {{-- <span>
-                                                    <i class="fa fa-user" aria-hidden="true"></i>
-                                                    <a href='author.php'>Admin</a>
-                                                </span> --}}
-                                                <span>
-                                                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                    {{ $dat->created_at }}
-                                                </span>
+                                        <div class="col-md-4">
+                                            <a class="post-img" href=""><img src="{{ asset('images/' . $dat->image)}}" alt="error"/></a>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="inner-content clearfix">
+                                                <h3><a href='single.php'>{{ $dat->title }}</a></h3>
+                                                <div class="post-information">
+                                                    <span>
+                                                        <i class="fa fa-tags" aria-hidden="true"></i>
+                                                        <a href='category.php'>{{ $dat->categories->categories_name }}</a>
+                                                    </span>
+                                                    <span>
+                                                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                        {{ $dat->created_at }}
+                                                    </span>
+                                                </div>
+                                                <p class="description">
+                                                    {{ $dat->description }}
+                                                </p>
+                                                <a class='read-more pull-right' href='{{ route('news-web.show' , $dat->id ) }}'>read more</a>
                                             </div>
-                                            <p class="description">
-                                                {{ $dat->description }}
-                                            </p>
-                                            <a class='read-more pull-right' href='{{ route('news-web.show' , $dat->id ) }}'>read more</a>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-
+                            @else
+                                <div class="post-content">
+                                    <p>No image available for this post</p>
+                                </div>
+                            @endif
                         @endforeach
 
                         <ul class='pagination admin-pagination'>
